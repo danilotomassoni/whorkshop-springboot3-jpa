8dcb93c6f4fb04beeb0570b0c9c434539f26f874
danilotomassoni <daniloaugustotomassoni@gmail.com>
2025-03-09 18:58:04 -0300

    JPA

--- src/main/java/com/curse/demo/models/User.java
+++ src/main/java/com/curse/demo/models/User.java
@@ -2,12 +2,23 @@ package com.curse.demo.models;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_users")
 public class User implements Serializable{
 
     @SuppressWarnings("unused")
     private static final Long serialVersionUID = 1L;
-
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String fone;
