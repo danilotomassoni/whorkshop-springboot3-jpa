2b3d6dcbb277ceed97006c37e752a74024af8e15
danilotomassoni <daniloaugustotomassoni@gmail.com>
2025-03-11 19:42:26 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/curse/demo/models/Order.java
@@ -0,0 +1,40 @@
+package com.curse.demo.models;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_orders")
+public class Order implements Serializable{
+
+    @SuppressWarnings("unused")
+    private static final Long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name="client_id")
+    private User client;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.client = client;
+    }
+
+    
+}
--- src/main/java/com/curse/demo/models/User.java
+++ src/main/java/com/curse/demo/models/User.java
@@ -1,11 +1,14 @@
 package com.curse.demo.models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +27,8 @@ public class User implements Serializable{
     private String fone;
     private String password;
 
+    @OneToMany(mappedBy="client")
+    private List<Order> orders = new ArrayList<>();
     public User() {
     }
 
@@ -34,6 +39,12 @@ public class User implements Serializable{
         this.fone = fone;
         this.password = password;
     }
+    
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
 
     public Long getId() {
         return id;
