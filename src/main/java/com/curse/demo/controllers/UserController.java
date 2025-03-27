723307240c1f55bb064d9b85cd59b9a143262eb8
danilotomassoni <daniloaugustotomassoni@gmail.com>
2025-03-27 19:42:50 -0300

    User insert

--- src/main/java/com/curse/demo/controllers/UserController.java
+++ src/main/java/com/curse/demo/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.curse.demo.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.curse.demo.models.User;
 import com.curse.demo.services.UserService;
+
 
 
 @RestController
@@ -33,5 +38,13 @@ public class UserController {
 
         return ResponseEntity.status(HttpStatus.OK).body(obj);
     }
+    @PostMapping    
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(user);
+    }
+    
     
 }
--- src/main/java/com/curse/demo/services/UserService.java
+++ src/main/java/com/curse/demo/services/UserService.java
@@ -20,5 +20,8 @@ public class UserService {
     public User findById(Long id){
         return userRepository.findById(id).get();
     }
+    public User insert(User obj){
+        return userRepository.save(obj);
+    }
 
 }
