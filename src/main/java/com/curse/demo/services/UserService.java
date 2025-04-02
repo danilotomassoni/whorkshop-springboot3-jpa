37aec678d4a3d84ae739e16e9b1ce230937f61db
danilotomassoni <daniloaugustotomassoni@gmail.com>
2025-04-02 19:04:08 -0300

    Exception handling - findById

--- src/main/java/com/curse/demo/services/UserService.java
+++ src/main/java/com/curse/demo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.curse.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.curse.demo.models.User;
 import com.curse.demo.repositories.UserRepository;
+import com.curse.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -18,7 +20,8 @@ public class UserService {
         return userRepository.findAll();
     }
     public User findById(Long id){
-        return userRepository.findById(id).get();
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(()->new ResourceNotFoundException(id));
     }
     public User insert(User obj){
         return userRepository.save(obj);
@@ -33,7 +36,7 @@ public class UserService {
         entity.setName(obj.getName());
         entity.setEmail(obj.getEmail());
         entity.setFone(obj.getFone());
-        
+
         return userRepository.save(entity);
     }
 }
